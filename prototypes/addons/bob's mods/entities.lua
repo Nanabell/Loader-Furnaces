fffb95f8c37eadb05f000a35d720c76dfb911d31
Nanabell <marlon.delfin@googlemail.com>
2017-11-24 11:46:43 +0100

    Allow Loader Furnaces to work with their base counterpart

--- prototypes/addons/bob's mods/entities.lua
+++ prototypes/addons/bob's mods/entities.lua
@@ -1,10 +1,10 @@
 require("lib.entities")
 
 if data.raw.item["lf-furnace-04"] then
-    data:extend({add_furnace_entity("lf-furnace-04", {"lf-smelting"}, 53.333 * 3.5, 180 * 48 * 8)})
+    data:extend({add_furnace_entity("lf-furnace-04", {"smelting", "lf-smelting"}, 53.333 * 3.5, 180 * 48 * 8)})
 
     if data.raw.item["lf-furnace-05"] then
-        data:extend({add_furnace_entity("lf-furnace-05", {"lf-smelting"}, 66.666 * 3.5, 180 * 48 * 16)})
+        data:extend({add_furnace_entity("lf-furnace-05", {"smelting", "lf-smelting"}, 66.666 * 3.5, 180 * 48 * 16)})
     end
 end
 
@@ -12,13 +12,23 @@ if data.raw.item["lf-chemical-01"] then
     data:extend(
         {
             --Chemical
-            add_furnace_entity("lf-chemical-01", {"lf-chemical-smelting"}, 13.333 * 3.5, 180 * 48),
-            add_furnace_entity("lf-chemical-02", {"lf-chemical-smelting"}, 13.333 * 3.5, 180 * 48 * 2),
-            add_furnace_entity("lf-chemical-03", {"lf-chemical-smelting"}, 13.333 * 3.5, 180 * 48 * 4),
+            add_furnace_entity("lf-chemical-01", {"chemical-furnace", "lf-chemical-smelting"}, 13.333 * 3.5, 180 * 48),
+            add_furnace_entity(
+                "lf-chemical-02",
+                {"chemical-furnace", "lf-chemical-smelting"},
+                13.333 * 3.5,
+                180 * 48 * 2
+            ),
+            add_furnace_entity(
+                "lf-chemical-03",
+                {"chemical-furnace", "lf-chemical-smelting"},
+                13.333 * 3.5,
+                180 * 48 * 4
+            ),
             --Mixing
-            add_furnace_entity("lf-mixing-01", {"lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48),
-            add_furnace_entity("lf-mixing-02", {"lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48 * 2),
-            add_furnace_entity("lf-mixing-03", {"lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48 * 4)
+            add_furnace_entity("lf-mixing-01", {"mixing-furnace", "lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48),
+            add_furnace_entity("lf-mixing-02", {"mixing-furnace", "lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48 * 2),
+            add_furnace_entity("lf-mixing-03", {"mixing-furnace", "lf-mixing-smelting"}, 13.333 * 3.5, 180 * 48 * 4)
         }
     )
 
@@ -26,9 +36,19 @@ if data.raw.item["lf-chemical-01"] then
         data:extend(
             {
                 --Chemical
-                add_furnace_entity("lf-chemical-04", {"lf-chemical-smelting"}, 53.333 * 3.5, 180 * 48 * 8),
+                add_furnace_entity(
+                    "lf-chemical-04",
+                    {"chemical-furnace", "lf-chemical-smelting"},
+                    53.333 * 3.5,
+                    180 * 48 * 8
+                ),
                 --Mixing
-                add_furnace_entity("lf-mixing-04", {"lf-mixing-smelting"}, 53.333 * 3.5, 180 * 48 * 8)
+                add_furnace_entity(
+                    "lf-mixing-04",
+                    {"mixing-furnace", "lf-mixing-smelting"},
+                    53.333 * 3.5,
+                    180 * 48 * 8
+                )
             }
         )
 
@@ -36,9 +56,19 @@ if data.raw.item["lf-chemical-01"] then
             data:extend(
                 {
                     --Chemical
-                    add_furnace_entity("lf-chemical-05", {"lf-chemical-smelting"}, 66.666 * 3.5, 180 * 48 * 16),
+                    add_furnace_entity(
+                        "lf-chemical-05",
+                        {"chemical-furnace", "lf-chemical-smelting"},
+                        66.666 * 3.5,
+                        180 * 48 * 16
+                    ),
                     --Mixing
-                    add_furnace_entity("lf-mixing-05", {"lf-mixing-smelting"}, 66.666 * 3.5, 180 * 48 * 16)
+                    add_furnace_entity(
+                        "lf-mixing-05",
+                        {"mixing-furnace", "lf-mixing-smelting"},
+                        66.666 * 3.5,
+                        180 * 48 * 16
+                    )
                 }
             )
         end
--- prototypes/vanilla/entities.lua
+++ prototypes/vanilla/entities.lua
@@ -2,8 +2,8 @@ require("lib.entities")
 
 data:extend(
     {
-        add_furnace_entity("lf-furnace-01", {"lf-smelting"}, 13.333 * 3.5, 180 * 48),
-        add_furnace_entity("lf-furnace-02", {"lf-smelting"}, 26.666 * 3.5, 180 * 48 * 2),
-        add_furnace_entity("lf-furnace-03", {"lf-smelting"}, 40 * 3.5, 180 * 48 * 4)
+        add_furnace_entity("lf-furnace-01", {"smelting", "lf-smelting"}, 13.333 * 3.5, 180 * 48),
+        add_furnace_entity("lf-furnace-02", {"smelting", "lf-smelting"}, 26.666 * 3.5, 180 * 48 * 2),
+        add_furnace_entity("lf-furnace-03", {"smelting", "lf-smelting"}, 40 * 3.5, 180 * 48 * 4)
     }
 )
