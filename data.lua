bbcbe90fcafc89527e365ed1e244bfb6c558375a
Nanabell <marlon.delfin@googlemail.com>
2018-10-06 15:12:21 +0200

    Update data.lua to auto discover recipes

--- data.lua
+++ data.lua
@@ -1,13 +1,32 @@
 require("prototypes.category")
 
---vanilla
-require("prototypes.vanilla.items")
-require("prototypes.vanilla.entities")
-require("prototypes.vanilla.recipes")
-require("prototypes.vanilla.technologies")
+require("prototypes.items")
+require("prototypes.entities")
+require("prototypes.recipes")
+require("prototypes.technologies")
 
---bob's mods
-require("prototypes.addons.bob's mods.items")
-require("prototypes.addons.bob's mods.entities")
-require("prototypes.addons.bob's mods.recipes")
-require("prototypes.addons.bob's mods.technologies")
+require("lib.recipes")
+require("lib.table")
+
+counter = 1
+for _, recipe in pairs(data.raw["recipe"]) do
+  if recipe.category == "smelting" then
+    counter = counter + 1
+    log('Found smelting recipe: "' + recipe.name + '"')
+
+    ingredients = get_ingredients(recipe, false)
+    output = get_result(recipe, false)
+    if output.result then
+      output.name = "lf-" + recipe.name
+
+      table.each(ingredients, function(v) v[2] = v[2] * math.ceil(2 / output.result_count) end)
+      output.result_count = 2
+      
+      --log('adding lf-smelting recipe "' + output.name + '" with ingredients = ' + serpent.line(ingredients) + ' and result = ' + serpent.line(output))
+      data:extend({add_smelting_recipe(output, {category = "lf-smelting", subgroup = "lf-smelting"}, ingredients, 2)})
+    else
+      log("recipe -" + recipe.anme + "- has more than one output. Recipe will be ignored")
+    end
+  end
+end
+log(counter + " total smelting recipes")
