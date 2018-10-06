18e571887af73109b0cd122b07242b7ea784ec75
Nanabell <marlon.delfin@googlemail.com>
2018-10-06 15:11:59 +0200

    Respect Belt speed changes by bobs log. overhaul

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,16 @@
---bob's mods
-require("prototypes.addons.bob's mods.updates")
+counter = 1
+for _, recipe in pairs(data.raw["recipe"]) do
+  if recipe.category == "smelting" then
+    counter = counter + 1
+  end
+end
+log(counter + " total smelting recipes")
+
+-- fix bobs stupid belt speed changes in data-updates
+if settings.startup["bobmods-logistics-beltoverhaul"] and settings.startup["bobmods-logistics-beltoverhaul"].value == true then
+  data.raw["assembling-machine"]["lf-furnace-01"].crafting_speed = (data.raw["transport-belt"]["transport-belt"].speed * 640) / 1.5
+  data.raw["assembling-machine"]["lf-furnace-02"].crafting_speed = (data.raw["transport-belt"]["fast-transport-belt"].speed * 640) / 1.5
+  data.raw["assembling-machine"]["lf-furnace-03"].crafting_speed = (data.raw["transport-belt"]["express-transport-belt"].speed * 640) / 1.5
+  data.raw["assembling-machine"]["lf-furnace-04"].crafting_speed = (data.raw["transport-belt"]["turbo-transport-belt"].speed * 640) / 1.5
+  data.raw["assembling-machine"]["lf-furnace-05"].crafting_speed = (data.raw["transport-belt"]["ultimate-transport-belt"].speed * 640) / 1.5
+end
