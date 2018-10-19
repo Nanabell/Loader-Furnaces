6991a0b4e7a3c28d4409a26e6c12c72de6f58ac8
Nanabell <marlon.delfin@googlemail.com>
2018-10-19 22:47:07 +0200

    Use zipfile Python module to build Mod Zip

--- build.py
+++ build.py
@@ -1,8 +1,26 @@
-from subprocess import call
+import zipfile
 import json
+import os
+
+exclude = [".gitignore", ".idea", ".vscode", ".git", "build.py"]
 
 with open('info.json') as info:
     version = json.load(info)["version"]
+folder = 'Loader-Furnaces_' + version
+archive = folder + '.zip'
 
-archive = "../Loader-Furnaces_" + version + ".zip"
-call(["git", "archive", "-o", archive, "HEAD"])
+
+def zip_files(zipper: zipfile.ZipFile):
+    for root, dirs, files in os.walk('Loader-Furnaces'):
+        dirs[:] = [d for d in dirs if d not in exclude]
+        files[:] = [f for f in files if f not in exclude]
+
+        for file in files:
+            print(os.path.join(root, file))
+            zipper.write(os.path.join(root, file))
+
+
+os.chdir('../')
+zipper = zipfile.ZipFile(archive, 'w', zipfile.ZIP_DEFLATED)
+zip_files(zipper)
+zipper.close()
