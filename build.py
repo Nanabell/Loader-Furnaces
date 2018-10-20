21b09efaa3d64355587a376b9dfcd7075db128bc
Nanabell <marlon.delfin@googlemail.com>
2018-10-20 16:16:08 +0200

    modify build.py to use methods

--- build.py
+++ build.py
@@ -1,26 +1,37 @@
-import zipfile
 import json
 import os
+import zipfile
 
-exclude = [".gitignore", ".idea", ".vscode", ".git", "build.py"]
-
-with open('info.json') as info:
-    version = json.load(info)["version"]
-folder = 'Loader-Furnaces_' + version
-archive = folder + '.zip'
+excludes = [".gitignore", ".idea", ".vscode", ".git", "build.py", "deploy.py"]
 
 
-def zip_files(zipper: zipfile.ZipFile):
+def get_archive_name(basedir: str):
+    with open(basedir + '/info.json') as info:
+        version = json.load(info)['version']
+    return 'Loader-Furnaces_' + version + '.zip'
+
+
+def get_archive_path(basedir: str):
+    return os.path.abspath(get_archive_name(basedir))
+
+
+def zip_files():
+    os.chdir("../")
+
+    zipper = zipfile.ZipFile(get_archive_name('loader-Furnaces'), 'w', zipfile.ZIP_DEFLATED)
+    output_file = os.path.abspath(zipper.filename)
+
     for root, dirs, files in os.walk('Loader-Furnaces'):
-        dirs[:] = [d for d in dirs if d not in exclude]
-        files[:] = [f for f in files if f not in exclude]
+        dirs[:] = [d for d in dirs if d not in excludes]
+        files[:] = [f for f in files if f not in excludes]
 
         for file in files:
-            print(os.path.join(root, file))
+            print('Zipping: ' + os.path.join(root, file))
             zipper.write(os.path.join(root, file))
 
+    zipper.close()
+    os.chdir("Loader-Furnaces")
+    return output_file
 
-os.chdir('../')
-zipper = zipfile.ZipFile(archive, 'w', zipfile.ZIP_DEFLATED)
-zip_files(zipper)
-zipper.close()
+
+print(zip_files())
